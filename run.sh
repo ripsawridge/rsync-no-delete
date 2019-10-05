7062372407f546a86cf3ed8271c5ba0ddb308a1a
Mike Stanton <mvstanton@chromium.org>
2019-10-05 17:22:17 +0200

    better

--- run.sh
+++ run.sh
@@ -2,48 +2,48 @@
 set -e
 
 # host option
-if [ -z "$WERCKER_RSYNC_DEPLOY_HOST" ]
+if [ -z "$WERCKER_RSYNC_NO_DELETE_HOST" ]
 then
     fail 'missing host option, please add this the rsync-deploy step in wercker.yml'
 fi
 
 # directory option
-if [ -z "$WERCKER_RSYNC_DEPLOY_DIRECTORY" ]
+if [ -z "$WERCKER_RSYNC_NO_DELETE_DIRECTORY" ]
 then
     fail 'missing directory option, please add this the rsync-deploy step in wercker.yml'
 fi
 
 # user option
 remote_user="ubuntu"
-if [ -n "$WERCKER_RSYNC_DEPLOY_USER" ]; # Check $WERCKER_BUNDLE_INSTALL exists and is not empty
+if [ -n "$WERCKER_RSYNC_NO_DELETE_USER" ]; # Check $WERCKER_BUNDLE_INSTALL exists and is not empty
 then
-    remote_user="$WERCKER_RSYNC_DEPLOY_USER"
+    remote_user="$WERCKER_RSYNC_NO_DELETE_USER"
 fi
 info "using user $remote_user"
 
 # port option
 remote_port="22"
-if [ -n "$WERCKER_RSYNC_DEPLOY_SSHPORT" ]; # Check $WERCKER_RSYNC_DEPLOY_SSHPORT exists and is not empty
+if [ -n "$WERCKER_RSYNC_NO_DELETE_SSHPORT" ]; # Check $WERCKER_RSYNC_DEPLOY_SSHPORT exists and is not empty
 then
-    remote_port="$WERCKER_RSYNC_DEPLOY_SSHPORT"
+    remote_port="$WERCKER_RSYNC_NO_DELETE_SSHPORT"
 fi
 info "using remote port $remote_port"
 
 # key option
 rsync_command="ssh -o BatchMode=yes -p $remote_port" # Batchmode to prevent it from waiting on user input
-if [ -n "$WERCKER_RSYNC_DEPLOY_SSHKEY" ]
+if [ -n "$WERCKER_RSYNC_NO_DELETE_SSHKEY" ]
 then
-    rsync_command="$rsync_command -i $WERCKER_RSYNC_DEPLOY_SSHKEY"
+    rsync_command="$rsync_command -i $WERCKER_RSYNC_NO_DELETE_SSHKEY"
 fi
 
 source_dir="./"
-if [ -n "$WERCKER_RSYNC_DEPLOY_SOURCE" ]; # check if source dir is specified
+if [ -n "$WERCKER_RSYNC_NO_DELETE_SOURCE" ]; # check if source dir is specified
 then
-    source_dir=$WERCKER_RSYNC_DEPLOY_SOURCE
+    source_dir=$WERCKER_RSYNC_NO_DELETE_SOURCE
 fi
 
-info "Synchronizing $source_dir to $remote_user@$WERCKER_RSYNC_DEPLOY_HOST:$WERCKER_RSYNC_DEPLOY_DIRECTORY..."
-sync_output=$(rsync -urltv --rsh="$rsync_command" "$source_dir" "$remote_user@$WERCKER_RSYNC_DEPLOY_HOST:$WERCKER_RSYNC_DEPLOY_DIRECTORY")
+info "Synchronizing $source_dir to $remote_user@$WERCKER_RSYNC_NO_DELETE_HOST:$WERCKER_RSYNC_NO_DELETE_DIRECTORY..."
+sync_output=$(rsync -urltv --rsh="$rsync_command" "$source_dir" "$remote_user@$WERCKER_RSYNC_NO_DELETE_HOST:$WERCKER_RSYNC_NO_DELETE_DIRECTORY")
 if [[ $? -ne 0 ]];then
     warning $sync_output
     fail 'rsync failed';
